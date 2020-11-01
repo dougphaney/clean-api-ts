51314cce03e133490945ea051f562143cf5ed55e
dougphaney <dougphaney@hotmail.com>
2020-11-01 01:41:04 -0300

    refactor: restore last version before mongodb connection

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -2,6 +2,7 @@ module.exports = {
   mongodbMemoryServerOptions: {
     binary: {
       version: '3.6.2',
+      // version: '4.0.3',
       skipMD5: true
     },
     autoStart: false,
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -2,8 +2,11 @@ import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
+  // uri: null as string,
 
   async connect (uri: string): Promise<void> {
+    // this.uri = uri
+    // this.client = await MongoClient.connect(uri, {
     this.client = await MongoClient.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true
