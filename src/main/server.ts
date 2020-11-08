09050e3dfee97893144b1295604c755bbaed6ac6
dougphaney <dougphaney@hotmail.com>
2020-11-07 22:58:45 -0300

    chore: update dependencies

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,12 +1,12 @@
-// import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
-// import env from './config/env'
+// import app from './config/app'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import env from './config/env'
 
-// MongoHelper.connect(env.mongoUrl)
-//   .then(async () => {
-//     const app = (await import('./config/app')).default
-//     app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
-//   })
-//   .catch(console.error)
-import app from './config/app'
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+  })
+  .catch(console.error)
 
-app.listen(5050, () => console.log('Server running at http://localhost:5050'))
+// app.listen(5050, () => console.log('Server running at http://localhost:5050'))
